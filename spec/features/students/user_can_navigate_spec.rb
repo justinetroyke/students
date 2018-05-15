cac5b43d642010550f41534d94a17ef5721af0e1
Justine <jjtroyke@gmail.com>
2018-05-15 15:53:41 -0600

    test for all and new student links on new and edit pages

--- spec/features/students/user_can_navigate_spec.rb
+++ spec/features/students/user_can_navigate_spec.rb
@@ -29,7 +29,45 @@ describe 'User sees navigation links on all pages' do
     visit student_path(student)
     expect(page).to have_link(link_name)
     expect(page).to have_link(link_name_2)
-    
+
+    click_link(link_name)
+
+    expect(current_path).to eq students_path
+
+    click_link(link_name_2)
+
+    expect(current_path).to eq new_student_path
+  end
+
+  scenario 'sees all and create students links on edit page' do
+    name = 'Jeffrey Lebowski'
+    link_name = 'All Students'
+    link_name_2 = 'Create New Student'
+    student = Student.create!(name: name)
+
+    visit edit_student_path(student)
+    expect(page).to have_link(link_name)
+    expect(page).to have_link(link_name_2)
+
+    click_link(link_name)
+
+    expect(current_path).to eq students_path
+
+    click_link(link_name_2)
+
+    expect(current_path).to eq new_student_path
+  end
+
+  scenario 'sees all and create students links on new page' do
+    name = 'Jeffrey Lebowski'
+    link_name = 'All Students'
+    link_name_2 = 'Create New Student'
+    Student.create!(name: name)
+
+    visit new_student_path
+    expect(page).to have_link(link_name)
+    expect(page).to have_link(link_name_2)
+
     click_link(link_name)
 
     expect(current_path).to eq students_path
