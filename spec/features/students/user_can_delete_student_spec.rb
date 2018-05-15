b337beabc9dae59a0743ebe331ea92bec5046ed8
Justine <jjtroyke@gmail.com>
2018-05-15 15:49:23 -0600

    all and new student links added to student index

--- spec/features/students/user_can_delete_student_spec.rb
+++ spec/features/students/user_can_delete_student_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe 'User creates delete student through /stduents' do
+describe 'User creates delete student through /students' do
   scenario 'sees index without deleted student' do
     name = 'Jeffrey Lebowski'
-    student = Student.create!(name: name)
+    Student.create!(name: name)
 
     visit students_path
     expect(page).to have_content(name)
@@ -14,9 +14,3 @@ describe 'User creates delete student through /stduents' do
     expect(current_path).to eq students_path
   end
 end
-
-# As a user
-# When I visit `/students`
-# And I click "Delete" next to a student's name
-# I see the students index
-# And that student's name is no longer on the page
---
+++ spec/features/students/user_can_navigate_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'User sees navigation links on all pages' do
+  scenario 'sees all and create students links on index page' do
+    name = 'Jeffrey Lebowski'
+    link_name = 'All Students'
+    link_name_2 = 'Create New Student'
+    Student.create!(name: name)
+
+    visit students_path
+    expect(page).to have_link(link_name)
+    expect(page).to have_link(link_name_2)
+
+    click_link(link_name)
+
+    expect(current_path).to eq students_path
+
+    click_link(link_name_2)
+
+    expect(current_path).to eq new_student_path
+  end
+end
+
+# As a user
+# When I visit any page
+# I see links to see a list of all students,
+# or create a new student
